3d636a8e50359488b2ed37ecd75dd4548f6ce614
Marcellino Ornelas <marcellino.ornelas.com>
2018-06-11 18:08:18 -0700

    added path module

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 var express = require('express');
 var app = express();
+var path = require('path');
 
 
 app.use( express.static( __dirname + '/public') );
 
 app.get('/', function(req,res){
-	res.sendFile( __dirname + '/public/html/home.html');
+	res.sendFile( path.join(__dirname,'/public/html/home.html') );
 })
 
 
