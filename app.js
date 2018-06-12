1125dee713f555818c87a899f457094023c2806d
Marcellino Ornelas <marcellino.ornelas.com>
2018-06-11 18:18:37 -0700

    fixed sendFile function in app.jus

--- app.js
+++ app.js
@@ -6,7 +6,18 @@ var path = require('path');
 app.use( express.static( path.join(__dirname,'/public') ) );
 
 app.get('/', function(req,res){
-	res.sendFile( './public/html/home.html' );
+	var options = {
+	    root: __dirname + '/public/html/',
+	    dotfiles: 'deny',
+	    headers: {
+	        'x-timestamp': Date.now(),
+	        'x-sent': true
+	    }
+  	};
+
+  var fileName = req.params.name;
+
+  res.sendFile( 'home.html', options);
 })
 
 
